35ea7a61aa9a35cae21d8a9e912094beb40631f6
leec89 <leec89@gmail.com>
2022-04-08 15:41:59 -0500

    finished Conditional exercises

--- js/conditionals.js
+++ js/conditionals.js
@@ -218,3 +218,43 @@ calculateTotal(randomWalmartLuckyNum, subTotalAmount);
  * Can you refactor your code to use functions?
  * HINT: The way we prompt for a value could be improved
  */
+
+function checkIfNaN(checkForNaN) {
+    if (isNaN(checkForNaN)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkOddEven(isItOddEven) {
+    if (isItOddEven % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
+
+function addHundred(toAdd100) {
+    return 100 + toAdd100;
+}
+
+function checkNegPos(isItOddPos) {
+    if (isItOddPos < 0) {
+        return 'negative';
+    } else {
+        return 'positive';
+    }
+}
+
+var askForNumber = confirm('Would you like to enter a number?');
+if (askForNumber) {
+    var promptNumber = Number(prompt('Please enter a number'));
+    if (checkIfNaN(promptNumber)) {
+        alert('Sorry, that\'s not a number!')
+    } else {
+        alert('It\'s an ' + checkOddEven(promptNumber) + ' number.');
+        alert('Adding 100 to your number gives you ' + addHundred(promptNumber) + '.');
+        alert('It\'s a ' + checkNegPos(promptNumber) + ' number.');
+    }
+}
