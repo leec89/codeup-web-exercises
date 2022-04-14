242d9e5b6759d40888034089e58509aac4577e79
leec89 <leec89@gmail.com>
2022-04-13 21:45:05 -0500

    array curriculum exercises - 90% complete

--- js/iterating.js
+++ js/iterating.js
@@ -71,4 +71,8 @@
         return console.log(inputArr[inputArr.length-1]);
     }
 
+first([1, 2, 3, 4, 5]) // returns 1
+second([1, 2, 3, 4, 5]) // returns 2
+last([1, 2, 3, 4, 5]) // return 5
+
 })();
