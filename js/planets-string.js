a24ba926ba0a2ad7a30bad93f4e4e5f121eb24fa
leec89 <leec89@gmail.com>
2022-04-14 21:29:15 -0500

    string manipulation curriculum exercises

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,4 +1,4 @@
-(function(){
+// (function(){
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
@@ -11,17 +11,44 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    planetsArray = planetsString.split('|');
+    console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      *
+     * */
+
+    var planetsWithBreakTags = planetsArray.join('<br>')
+    console.log(planetsWithBreakTags);
+
+    /**
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
 
-})();
+    const list = document.querySelector('.output ul');
+// list.innerHTML = '';
+const greetings = ['Happy Birthday!',
+    'Merry Christmas my love',
+    'A happy Christmas to all the family',
+    'You\'re all I want for Christmas',
+    'Get well soon'];
+
+for (let greeting of greetings) {
+    // Your conditional test needs to go inside the parentheses
+    // in the line below, replacing what's currently there
+    if (greeting) {
+        const listItem = document.createElement('li');
+        listItem.textContent = greeting;
+        // list.appendChild(listItem);
+    }
+}
+
+
+
+// })();
