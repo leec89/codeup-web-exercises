2e59613fbd39fc0f8f7898cf60c73f335ea956be
leec89 <leec89@gmail.com>
2022-04-14 21:30:29 -0500

    advanced array exercises

---
+++ js/advancedArrays.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var pies = [
+    "apple",
+    "cherry",
+    "key lime",
+    "huckleberry",
+    "rhubarb"
+];
+
+function lastThreePies(pies) {
+    return pies.slice(pies.length-3);
+}
+
+console.log(lastThreePies(pies));
+
+var stringTest = 'Test this string'
+var stringCopy = stringTest;
+stringCopy.toUpperCase();
+var test = stringTest.toUpperCase();
+
+console.log(stringTest);
+console.log(stringCopy);
+console.log(test)
