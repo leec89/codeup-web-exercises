193271b7b083bf48a7e4733fbbfffa1434e5acb9
leec89 <leec89@gmail.com>
2022-04-15 17:15:47 -0500

    completed array bonus for practice

--- js/d10w03_arrayBonus.js
+++ js/d10w03_arrayBonus.js
@@ -18,28 +18,79 @@
        5125553030
 */
 
-var phoneNumbers = '210-555-2020\n230-555-2020\n512-555-3030';
+var phoneNums = '210-555-2020\n230-555-2020\n512-555-3030';
 function cleanPhoneNumbers(phoneNumbers) {
     var scrubNumbers = phoneNumbers.split('\n');
     for (var x in scrubNumbers) {
-        console.log()
+        console.log(scrubNumbers[x]);
     }
 }
+cleanPhoneNumbers(phoneNums);
+
+function cleanPhoneNumsForEach (arrPhoneNums) {
+    var scrubbedNums = arrPhoneNums.split('\n');
+    scrubbedNums.forEach(function (phoneNum) {
+        console.log(phoneNum);
+    })
+}
+cleanPhoneNumsForEach(phoneNums);
+
+/*
 ============== BONUS 2
 
 Write a program to toggle all characters in a string: in a given string, toggle all characters to their opposite case. Special characters will not change.
 
     Example input: "aBc12#""
-Example output: "AbC12#"
+    Example output: "AbC12#"
+*/
+
+function toggleChars (inputStr) {
+    var outputChars = '';
+    var charArray = inputStr.split('');
+    charArray.forEach(function(char) {
+        char === char.toUpperCase()
+            ? outputChars += char.toLowerCase()
+            : outputChars += char.toUpperCase()
+    });
+    return outputChars;
+}
+
+console.log(toggleChars('aBc12#'));
 
 
+
+/*
 ============== BONUS 3
 
 Write a program that takes a given string appended with a number at the end. The program will check the length of the string to verify the string is equal to the number appended. If the number appended equals the string length, output "Yes". if the number appended does not equal the string length, output "No".
 
     Example Input: "geek5"
-Example Output: "Yes"
+    Example Output: "Yes"
 
-Example Input: "codingchallenge25"
+    Example Input: "codingchallenge25"
 
 */
+
+function findNum (givenStr) {
+    var outputNum = '';
+    var givenStrArr = givenStr.split('');
+    givenStrArr.forEach(function (checkEaCharIfNum) {
+        !isNaN(checkEaCharIfNum)
+            ? outputNum += checkEaCharIfNum
+            : false
+    })
+    return outputNum;
+}
+
+console.log(findNum('codingchallenge25'));
+
+
+function checkAndVerify (inputStr) {
+    var inputLenGiven = findNum(inputStr);
+    return inputStr.length === parseInt(inputLenGiven)
+        ? console.log('Yes, matches length')
+        : console.log('No, does not match length')
+}
+
+checkAndVerify("geek5");
+checkAndVerify("codingchallenge25")
