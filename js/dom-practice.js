5b3245659afb2185d526043af61d8d7b6ced4943
leec89 <leec89@gmail.com>
2022-05-15 16:56:38 -0500

    feat: DOM practice additions from Fri May 13 d29w07

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -2,27 +2,34 @@
 
 
 /** Thu May 12 - d28w07 ******************************************************************************************/
-var button = document.getElementById("changeColor");
-var color = document.querySelector("#color")
-var body = document.querySelector("body")
+
+(function () {
+    let button = document.getElementById("changeColor");
+    let color = document.querySelector("#color")
+    let body = document.querySelector("body")
+
 //
-function colorButton(e) {
-    // document.querySelector("body").style.backgroundColor = document.querySelector("#color").value
-    body.style.backgroundColor = color.value
-}
-button.addEventListener('click', colorButton)
+    function colorButton(e) {
+        // document.querySelector("body").style.backgroundColor = document.querySelector("#color").value
+        body.style.backgroundColor = color.value
+    }
+
+    button.addEventListener('click', colorButton)
 // document.getElementById("changeColor").addEventListener('click', colorButton)
+})();
 
 /** Fri May 13 - d29w07 ******************************************************************************************/
 
-var para = document.getElementById("doubleSize");
-var fontSizeParsed = parseFloat(window.getComputedStyle(document.getElementById("doubleSize")).fontSize);
+(function () {
+    let para = document.getElementById("doubleSize");
+    let fontSizeParsed = parseFloat(window.getComputedStyle(document.getElementById("doubleSize")).fontSize);
 
-function logSize(e) {
-    return para.style.fontSize = fontSizeParsed * 2 + 'px'
-}
+    function logSize(e) {
+        return para.style.fontSize = fontSizeParsed * 2 + 'px'
+    }
 
-para.addEventListener('dblclick', logSize);
+    para.addEventListener('dblclick', logSize);
+})();
 
 /** Fri May 13 - d29w07 *******DOM Practice Question #2**********************************************************/
 
@@ -56,15 +63,18 @@ para.addEventListener('dblclick', logSize);
  *
  */
 
-var button2 = document.getElementById("addTodo");
-var todoInput = document.querySelector("#todo")
-var ulList = document.querySelector("#todosList")
-//
-function colorButton2(e) {
-    ulList.innerHTML = '<li>todoInput.value</li>'
-}
-button2.addEventListener('click', colorButton2)
+(function () {
+    let button2 = document.getElementById("addTodo");
+    let todoInput = document.querySelector("#todo")
+    let ulList = document.querySelector("#todosList")
 
+//
+    function colorButton2(e) {
+        ulList.innerHTML = '<li>todoInput.value</li>'
+    }
+
+    button2.addEventListener('click', colorButton2)
+})();
 
 
 // DOM Practice Question #3
@@ -78,22 +88,48 @@ button2.addEventListener('click', colorButton2)
 // use that button to stop and restart the h1 tags from changing size.
 
 
-var button3 = document.getElementById("toggleH1");
-var h1All = document.querySelectorAll('h1')
+// let button3 = document.getElementById("toggleH1");
+// let allOfThem = document.getElementsByTagName('h1');
+//
+// function logSize2(e) {
+//     for (let i = 0; i < allOfThem.length; i++) {
+//         allOfThem[i].style.fontSize = 40 + 'px'
+//     }
+//
+// }
 
-function logSize2(e) {
-    return h1All.style.fontSize = '40px'
+(function () {
+var button3 = document.getElementById("toggleH1");
+console.log(button3)
+function myFunction() {
+    var fontSizeParsed = parseInt(window.getComputedStyle(button3).fontSize) + 5;
+    console.log(fontSizeParsed)
+
+    var allOfThem = document.getElementsByTagName('h1');
+    for (let i = 0; i < allOfThem.length; i++) {
+        if (parseFloat(allOfThem[i].style.fontSize) === fontSizeParsed) {
+            allOfThem[i].style.fontSize = fontSizeParsed + 40 + 'px';
+        } else {
+            allOfThem[i].style.fontSize = fontSizeParsed + 'px';
+        }
+    }
 }
 
-button3.addEventListener('click', logSize2);
-
-
+button3.addEventListener('click', myFunction);
+})();
 // DOM Question #4
 
 // When the mouse enters the content area of the 4th div with the class of "question", an alert should pop up
 // that reads "CONGRATULATIONS ON YOUR NEW CRUISE!";
 
-
+(function () {
+    var h1Tags = document.querySelector('.question');
+    console.log(h1Tags)
+    function myFunction() {
+        alert('CONGRATULATIONS ON YOUR NEW CRUISE!')
+    }
+    h1Tags.addEventListener('mouseover', myFunction);
+})();
 
 // DOM Question# #5
 
