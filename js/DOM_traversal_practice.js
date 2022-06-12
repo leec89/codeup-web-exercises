f94c15b7434dd9122ba128a1a2096bfa38c96bec
leec89 <leec89@gmail.com>
2022-06-12 12:39:22 -0500

    feat: DOM manipulation practice

--- js/DOM_traversal_practice.js
+++ js/DOM_traversal_practice.js
@@ -1,9 +1,29 @@
+const grandParent = document.querySelector(".grandparent")
+const parent = document.querySelector(".parent")
 const children = document.querySelectorAll(".child")
 
-// children.style.backgroundColor = "#333"
 
-children.forEach(changeColor)
-function changeColor(element) {
-    element.style.backgroundColor = "#999"
+// parents[1].style.backgroundColor = "#555"
+
+
+for (let i = 0; i < 2; i++) {
+    const newParentBlock = document.createElement("div")
+    newParentBlock.classList.add("parent")
+    newParentBlock.innerHTML =
+        `<div class="child"></div>
+     <div class="child"></div>`
+    grandParent.append(newParentBlock);
 }
 
+for (let i = 0; i < 2; i++) {
+    const newChild = document.createElement("div")
+    newChild.classList.add("child")
+    parent.append(newChild)
+}
+
+const parents = Array.from(document.getElementsByClassName("parent"))
+parents.forEach(changeColor)
+
+function changeColor(element) {
+    element.style.backgroundColor = "#555"
+}
